ec939a637fe6068d2655b30f1c37aea4e2ef9ef6
aungpor <napat.patc@ku.th>
2023-12-17 20:48:06 +0700

    first commit

---
+++ Readme.txt
@@ -0,0 +1,15 @@
+routes->controllers->service->repository
+
+routes
+ระบุเส้นทาง api อย่าลืม init controller อันใหม่
+
+controllers
+การทำงานของ api ทำนองว่ารับ parameter อะไรบ้าง response อะไร เอา function จาก service มาใช้งาน
+
+service
+การเอาข้อมูลที่เรียกมาจาก database มาทำงานเป็น function
+
+repository
+เรียกข้อมูลจาก database
+
+docker-compose up --build
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"TableTru/api/controller"
+	"TableTru/api/repository"
+	"TableTru/api/routes"
+	"TableTru/api/service"
+	"TableTru/infrastructure"
+	"TableTru/models"
+)
+
+func init() {
+    infrastructure.LoadEnv()
+}
+
+func main() {
+
+    router := infrastructure.NewGinRouter() //router has been initialized and configured
+    db := infrastructure.NewDatabase() // databse has been initialized and configured
+    postRepository := repository.NewPostRepository(db) // repository are being setup
+    postService := service.NewPostService(postRepository) // service are being setup
+    postController := controller.NewPostController(postService) // controller are being set up
+    postRoute := routes.NewPostRoute(postController, router) // post routes are initialized
+    postRoute.Setup() // post routes are being setup
+    db.DB.AutoMigrate(&models.Post{}) // migrating Post model to datbase table
+	
+	userRepository := repository.NewUserRepository(db)
+    userService := service.NewUserService(userRepository)
+    userController := controller.NewUserController(userService)
+    userRoute := routes.NewUserRoute(userController, router)
+    userRoute.Setup()
+    db.DB.AutoMigrate(&models.User{})
+
+    
+    router.Gin.Run(":8000") //server started on 8000 port
+}
