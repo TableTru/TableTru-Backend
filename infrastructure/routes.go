a87f0dc3a3792a287f3fe4d3ac08e4a7a9e3b189
aungpor <napat.patc@ku.th>
2023-12-27 12:29:09 +0700

    CORS fix

--- infrastructure/routes.go
+++ infrastructure/routes.go
@@ -4,6 +4,7 @@ import (
     "net/http"
 
     "github.com/gin-gonic/gin"
+    "github.com/gin-contrib/cors"
 )
 
 //GinRouter -> Gin Router
@@ -15,6 +16,18 @@ type GinRouter struct {
 func NewGinRouter() GinRouter {
 
     httpRouter := gin.Default()
+    
+    config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:3000"} // ระบุโดเมนที่ต้องการให้รับ CORS
+	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE"}
+	httpRouter.Use(cors.New(config))
+
+    // httpRouter.Use(cors.New(cors.Config{
+    //     AllowOrigins: []string{"http://localhost:3000"},
+    //     AllowMethods: []string{"POST", "PUT", "PATCH", "DELETE"},
+    //     AllowHeaders: []string{"Content-Type,access-control-allow-origin, access-control-allow-headers"},
+    // }))
+
 
     httpRouter.GET("/", func(c *gin.Context) {
         c.JSON(http.StatusOK, gin.H{"data": "Up and Running..."})
